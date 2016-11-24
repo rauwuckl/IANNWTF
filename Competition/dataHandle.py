3d14431b72551d5e0362f3b9da420984d63157e0
rauwuckl <ch.23@web.de>
2016-11-24 19:52:59 +0100

    bla

--- Competition/dataHandle.py
+++ Competition/dataHandle.py
@@ -6,12 +6,27 @@ class DataHandle:
         self.generalData = pickle.load(open("iannwtf-svhn/trainData.pickle", 'rb'))
         self.generalLabel = pickle.load(open("iannwtf-svhn/trainLabels.pickle", 'rb'))%10
 
-        self.indxValidation = np.random.choice(len(self.generalLabel), len(self.generalLabel)//8, replace=False)#
+        self.indxValidation = np.random.choice(len(self.generalLabel), len(self.generalLabel)//10, replace=False)#
         self.indxTraining = np.delete(range(len(self.generalLabel)), self.indxValidation)
 
+    """get random batches of size N
+    @return data, labels
+    where data is an array of shape [N, 28, 28, 1]  containing 28x28 gray images
+    and labels is of shape [N] containing ints in [0,10)
+    """
     def getTrainingBatch(self, N):
         indx = np.random.choice(self.indxTraining,N, replace=True)
         return self.generalData[indx], self.generalLabel[indx]
+
+    """see get TrainingBach"""
+    def getValidationIterator(self, N):
+        lower = 0
+        upper = 0
+        while lower<len(self.indxValidation):
+            upper = min(lower+N,len(self.indxValidation))
+            yield self.generalData[self.indxValidation[lower:upper]], self.generalLabel[self.indxValidation[lower:upper]]
+            lower = upper
+            
 
     def getValidationData(self):
         return self.generalData[self.indxValidation], self.generalLabel[self.indxValidation]
